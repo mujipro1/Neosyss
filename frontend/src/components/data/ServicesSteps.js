07fb5bb06a38686185d074d0de14574f1d82fe69
shaffayy <mshafay.bee21seecs@seecs.edu.pk>
2024-11-23 16:13:12 +0500

    added the changes requested

---
+++ frontend/src/components/data/ServicesSteps.js
@@ -0,0 +1,22 @@
+export const GIS = [
+    {
+        heading: "GIS / Cartography Solutions",
+        content: "We provide specialized solutions for real-world applications of GIS.",
+        image: "/Services/GIS.png",
+    },
+    {
+        heading: "GIS / Cartography Solutions",
+        content: "We provide specialized solutions for real-world applications of GIS.",
+        image: "/Services/GIS.png",
+    },
+    {
+        heading: "GIS / Cartography Solutions",
+        content: "We provide specialized solutions for real-world applications of GIS.",
+        image: "/Services/GIS.png",
+    },
+    {
+        heading: "GIS / Cartography Solutions",
+        content: "We provide specialized solutions for real-world applications of GIS.",
+        image: "/Services/GIS.png",
+    }
+]
